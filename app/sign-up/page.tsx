104f1cefeeb3e3aaf1786f5a820dd7fcdfbf0692
jon-jc <jonathancho.jc@gmail.com>
2024-06-06 11:15:01 -0700

    feat: add header component to notification, sign-in, and sign-up pages

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,9 +1,11 @@
+import Header from "@/components/header";
 import SignUpPage from "@/components/sign-up";
 import React from "react";
 
 const page = () => {
   return (
     <div>
+      <Header />
       <SignUpPage />
     </div>
   );
