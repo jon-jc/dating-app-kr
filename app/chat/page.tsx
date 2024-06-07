5305420c6aeb78bcc2de7f3ebd277ef4307bedf9
jon-jc <jonathancho.jc@gmail.com>
2024-06-07 14:00:22 -0700

    Implement Chat Component

---
+++ app/chat/page.tsx
@@ -0,0 +1,14 @@
+import Chat from "@/components/chat";
+import Header from "@/components/header";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <Header />
+      <Chat />
+    </div>
+  );
+};
+
+export default page;
